9ab89f68dbebced504eded0d7971afcfdb9d2489
Maykon Raphael <mraphaeld.batista@gmail.com>
2023-10-12 00:03:21 -0300

    feat: Create Card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { LabelComponent } from './components/card/label/label.component';
+import { PriceComponent } from './components/card/price/price.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CardComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    LabelComponent,
+    PriceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/card/price/price.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-price',
+  templateUrl: './price.component.html',
+  styleUrls: ['./price.component.css']
+})
+export class PriceComponent {
+
+  @Input()
+  gameType:string = ""
+  @Input()
+  gamePrice:string = ""
+}
